918c8ae36a48a6196a4b444e3030c7df1e982065
Istvan Tanko <tistvan1989@gmail.com>
2022-05-29 20:18:42 +0300

    3: Noteballs: Data, Methods, Child Components, Computed & Emit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,36 +3,73 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note"/>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
       <div class="field is-grouped is-grouped-right">
         <div class="control">
           <button
-            class="button is-link has-background-success">
-              Add New Note
-            </button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
           >
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
+
+    <Note
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+      @deleteClicked="deleteNote"
+    ></Note>
     
-    <div
-      v-for="i in 3"
-      class="card mb-4"
-    >
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut ea neque deleniti hic, soluta itaque, mollitia quae amet voluptatem harum quis praesentium sed recusandae quaerat quibusdam fugiat unde. Laudantium!
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
   </div>
 </template>
 
 <script setup>
+/*
+  imports
+*/
+import { ref } from 'vue';
+import Note from '@/components/notes/Note.vue'
+/*
+  notes
+*/
+const newNote = ref('')
+const newNoteRef = ref(null)
+const notes = ref([
+  {
+    id: 'id1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut ea neque deleniti hic, soluta itaque, mollitia quae amet voluptatem harum quis praesentium sed recusandae quaerat quibusdam fugiat unde. Laudantium!'
+  },
+  {
+    id: 'id2',
+    content: 'This is a shorter note! Woo!'
+  }
+])
+
+const addNote = () => {
+  let currentDate = new Date().getTime()
+  let id = currentDate.toString()
+  let note = {
+    id,
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+  newNote.value = ''
+  newNoteRef.value.focus()
+}
+
+const deleteNote = id => {
+  notes.value = notes.value.filter(note => { return note.id != id })
+}
 </script>
