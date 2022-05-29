5e69f0cfbdc4ee842ae382b3730f15abc34dfae3
Istvan Tanko <tistvan1989@gmail.com>
2022-05-29 18:16:20 +0300

    Section 1: Noteballs: Setup & Router

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="notes">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/ViewStats.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="stats">
+    <h1>Stats</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
