e0aaf19ffacf206f2417b10d449e20469f5ac450
Istvan Tanko <tistvan1989@gmail.com>
2022-05-29 19:17:41 +0300

    2: Noteballs: Bulma & Design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <router-link to="/">Notes | </router-link>
-  <router-link to="stats">Stats</router-link>
-  <router-view/>
+  <nav-bar/>
+  <div class="container is-max-desktop px-2 py-4">
+    <router-view/>
+  </div>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+import NavBar from '@/components/layout/NavBar.vue';
+
+
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"/>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button
+            class="button is-link has-background-success">
+              Add New Note
+            </button
+          >
+        </div>
+      </div>
+    </div>
+    
+    <div
+      v-for="i in 3"
+      class="card mb-4"
+    >
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut ea neque deleniti hic, soluta itaque, mollitia quae amet voluptatem harum quis praesentium sed recusandae quaerat quibusdam fugiat unde. Laudantium!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
 
